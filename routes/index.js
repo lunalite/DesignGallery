3315662d1f4eb84bf2c560ac9eaf0bf978f57954
hkoh006 <hd.koh290293@gmail.com>
2018-04-14 18:13:47 +0800

    added random

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+const Widget = mongoose.model('Widget');
+const _btnTypeArr = ["All", "CheckBox", "Chronometer", "CompoundButton", "EditText", "ImageButton", "ProgressBar", "RadioButton", "RatingBar", "SeekBar", "Spinner", "Switch", "ToggleButton", "View"];
+const _sortTypeDict = {
+    appDownloads: "Descending Number of Application Downloads",
+    appAlpbAsc: "Descending Alphabetical Order"
+};
+const displayPerPage = 30;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Mobile UI Gallery - Homepage'});
+    Widget.aggregate([{
+        $sample: {size: 30}
+    }]).exec(function (err, doc) {
+        if (err) {
+            return next(err);
+        }
+            res.render('index', {
+                title: 'Mobile UI Gallery - Homepage',
+                url: req.originalUrl,
+                btnTypeArr: _btnTypeArr,
+                sortTypeDict: _sortTypeDict,
+                widgets: doc,
+            });
+        });
 });
 
 module.exports = router;
